2936bdfbe88e99b7a3e0bc65483c170d666f3aba
five6516 <five6516@126.com>
2021-04-26 09:57:12 +0800

    add onSaveInstanceState test

--- DemoApp/app/src/main/java/com/flysnail/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/flysnail/demoapp/MainActivity.java
@@ -1,22 +1,43 @@
 package com.flysnail.demoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.flysnail.demoapp.layout.Layout_test;
 import com.flysnail.demoapp.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "com.flysnail.demoapp.MainActivity";
     public static final String EXTRA_MESSAGE = "com.flysnail.demoapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(null != savedInstanceState){
+            Log.i(TAG, "onCreate: savedInstanceState");
+        }
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.i(TAG, "onRestoreInstanceState");
     }
 
     public void layoutTest(View view){
